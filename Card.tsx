bf9ee827e8a4f57316b28aa1c2b33997ed462f55
brianleonel <112188352+brianleonel@users.noreply.github.com>
2025-03-18 16:27:54 -0300

    Card.tsx

--- Card.tsx
+++ Card.tsx
@@ -3,7 +3,7 @@ interface Props{/*se declara la estructura de la propiedad */
 }
 function Card(props: Props){/*argumentos q recibe la funcion, NombrePropiedad: tipoPropiedad */
   /*props se define como una interfaz con la estructura de Props*/
-  const {body}=props;
+  const {body}=props;// se hace una desestructuracion, se asigna props a body
   return (
   <>
     <h1>{body}</h1>
